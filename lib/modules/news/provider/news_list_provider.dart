1adddc32ba9f9354f12f990069179c57b56a9271
Bremade <mail@bremauer.cc>
2023-10-31 21:32:30 +0100

    TYS-31: Fixed dart analyze finding

--- lib/modules/news/provider/news_list_provider.dart
+++ lib/modules/news/provider/news_list_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:track_your_stop/modules/favorites/database/favorites_database.dart';
 import 'package:track_your_stop/modules/settings/provider/news_settings_provider.dart';
